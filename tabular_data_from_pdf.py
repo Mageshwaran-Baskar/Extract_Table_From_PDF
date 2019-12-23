a5cc093ec04305e6f6be44220f208d25b89ba3cf
Mageshwaran-Baskar <55185936+Mageshwaran-Baskar@users.noreply.github.com>
2019-12-23 09:01:18 +0530

    Add files via upload
    Commit

---
+++ tabular_data_from_pdf.py
@@ -0,0 +1,14 @@
+import os
+from tabula import read_pdf
+
+pdf_file = r"PDF file path .pdf"
+df = read_pdf(pdf_file, pages='all', multiple_tables=True)
+
+if not os.path.exists(os.path.splitext(pdf_file.split("/")[4])[0]):
+    os.mkdir(os.path.splitext(pdf_file.split("/")[4])[0])
+save_path = os.path.splitext(pdf_file.split("/")[4])[0] + '/'
+i = 1
+for data in df:
+    data.to_excel(save_path+"table_"+str(i)+".xlsx")
+    i = i+1
+
